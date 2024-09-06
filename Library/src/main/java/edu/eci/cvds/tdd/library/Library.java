2611b03cf87f2cf06a58a3464b568cca1020484c
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2024-09-06 00:08:10 -0500

    Solucion conflictos

--- Library/src/main/java/edu/eci/cvds/tdd/library/Library.java
+++ Library/src/main/java/edu/eci/cvds/tdd/library/Library.java
@@ -68,8 +68,6 @@ public class Library {
      * @return The new created loan.
      */
     public Loan loanABook(String userId, String isbn) {
-        // TODO Implement the login of loan a book to a user based on the UserId and the
-        // isbn.
         Book book = getBookWithIsbn(isbn);
         if (book == null) {
             throw new IllegalArgumentException("Book with ISBN " + isbn + " does not exist.");
